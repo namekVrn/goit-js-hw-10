a0efe2441622ef00ba24caf9c0cd69a0aaa0dee7
Andrey <gsmer.prof@yandex.ru>
2022-01-22 13:31:58 +0300

    re

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,13 @@
 export const fetchCountries = (country)=>{
-   
+  
     return fetch(`https:restcountries.com/v3.1/name/${country}?fields=capital,name,population,flags,languages`)
     .then((response)=>{
       if(!response.ok){
         throw new Error(response.status);
       }
+      console.log(response)
       return response.json()
+      
     })
 
     
