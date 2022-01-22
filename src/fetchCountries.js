22c11dc79add16eb34891d8642f7e16974e2e13c
Andrey <85876260+namekVrn@users.noreply.github.com>
2022-01-22 13:34:45 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
 export const fetchCountries = (country)=>{
   
-    return fetch(`https:restcountries.com/v3.1/name/${country}?fields=capital,name,population,flags,languages`)
+    return fetch(`https://restcountries.com/v3.1/name/${country}?fields=capital,name,population,flags,languages`)
     .then((response)=>{
       if(!response.ok){
         throw new Error(response.status);
