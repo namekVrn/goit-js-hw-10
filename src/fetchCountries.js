e4054b03789ccbbb554b85c612cbd179ccb04b72
Andrey <gsmer.prof@yandex.ru>
2022-01-22 12:36:30 +0300

    renew 10

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+export const fetchCountries = (country)=>{
+   
+    return fetch(`https:restcountries.com/v3.1/name/${country}?fields=capital,name,population,flags,languages`)
+    .then((response)=>{
+      if(!response.ok){
+        throw new Error(response.status);
+      }
+      return response.json()
+    })
+
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,38 +1,49 @@
 import './css/styles.css';
-// import {fetchCountries} from './fetchCountries.js'
+import {fetchCountries} from './fetchCountries'
 import debounce from 'lodash.debounce'
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box')
 const ul = document.querySelector('.country-list')
+
 input.addEventListener('input', debounce((e)=>{
-    let serchCountry = e.target.value
+    let serchCountry = e.target.value.trim()
     console.log(serchCountry)
+    clearContainer()
     fetchCountries(serchCountry)
     .then((response)=>{
-        console.log(renderCountry(response))
+        clearContainer()
+        console.log(response)
+        if(response.length >= 10){
+            Notify.failure("Too many matches found. Please enter a more specific name.");
+            return 
+        }else if(response.length <= 10 && response.length > 1){
         let objCounrty = renderCountry(response)
+        console.log(objCounrty)
         ul.insertAdjacentHTML("beforeend",objCounrty)
-        
+        }
+        if(response.length === 1){
+            console.log('privet')
+            clearContainer()
+            const renderOneCounry = renderOnuCountry(response)
+            ul.insertAdjacentHTML("beforeend",renderOneCounry)
+        }
     })
-    .catch((error)=>{console.log(error)})
-}, 300))
-// +'?fields={name.official},{capital},{population}, {flags.svg},{languages}
-const fetchCountries = (country)=>{
-    let countries = '';
-    console.log(countries)
-    return fetch('https://restcountries.com/v3.1/name/'+country)
-    .then((response)=>{
-      if(!response.ok){
-        throw new Error(response.status);
-      }
-      return response.json()
+    
+    .catch((error)=>{
+        if(serchCountry === ""){
+            return
+        }
+        Notify.failure("нет такого");
     })
-}
+}, DEBOUNCE_DELAY))
 
-console.log(fetchCountries("peru"))
 
+
+function clearContainer(){
+    ul.innerHTML = ''
+};
 
 const renderCountry = (counryObj) =>{
     let resultCountry = counryObj.map((elem)=>{
@@ -44,4 +55,23 @@ const renderCountry = (counryObj) =>{
     return resultCountry
     console.log(resultCountry)
 }
+
+const renderOnuCountry = (counryObj) =>{
+    let resultCountry = counryObj.map((elem)=>{
+        return `<li class="itembox">
+        <img class="img_flag" src="${elem.flags.svg}" alt="flag">
+        <ul>
+            <li><span class="countryName">${elem.name.official}</span></li>
+            <li><span class="addOneCountry">Capital:${elem.capital}</span></li>
+            <li><span class="addOneCountry">Population:${elem.population}</span></li>
+            <li><span class="addOneCountry">Languages:${elem.languages.rus}</span></li>
+        </ul>
+        
+        <li>`
+    }).join('')
+    return resultCountry
+    console.log(resultCountry)
+}
+
+
 
